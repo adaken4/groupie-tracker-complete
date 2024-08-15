9d75da1fff7c300e1fbbaa76a620167efbc9ac98
adaken4 <adakennedy@outlook.com>
2024-08-15 11:11:02 +0300

    implementing the Artists struct to receive the JSON data for Artists

---
+++ backend/models.go
@@ -0,0 +1,19 @@
+package backend
+
+type Artists struct {
+	Id              int      `json:"id"`
+	ImageURL        string   `json:"image"`
+	Name            string   `json:"name"`
+	Members         []string `json:"members"`
+	CreationDate    int      `json:"creationDate"`
+	FirstAlbum      string   `json:"firstAlbum"`
+	LocationsURL    string   `json:"locations"`
+	ConcertDatesURL string   `json:"concertDates"`
+	RelationsURL    string   `json:"relations"`
+}
+
+type Locations struct{}
+
+type Dates struct{}
+
+type Relation struct{}

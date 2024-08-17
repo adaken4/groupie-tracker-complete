d0658b052f3f2262863c85ffcc69f5ed33dbaaad
adaken4 <adakennedy@outlook.com>
2024-08-17 14:06:12 +0300

    haven't changed the logic much, but still haven't implemented returning errors so they can be handled by the callers or up the chain

--- backend/utils.go
+++ backend/utils.go
@@ -3,27 +3,50 @@ package backend
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 )
 
-func PrintArtist(ID int) {
-	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+func GetAndUnmarshalArtists() []Artists {
+	artists := []Artists{}
+
+	artistsUrl := "https://groupietrackers.herokuapp.com/api/artists"
+	resp, err := http.Get(artistsUrl)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer resp.Body.Close()
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
 
-	var artists []Artists
-	err = json.Unmarshal(respBody, &artists)
-	for _, v := range artists {
-		if ID == v.ID {
-			fmt.Println(v)
+	if resp.StatusCode == http.StatusOK {
+
+		decoder := json.NewDecoder(resp.Body)
+		err = decoder.Decode(&artists)
+		if err != nil {
+			log.Fatalln(err)
 		}
 	}
+	return artists
+}
+
+func GetAndUnmarshalLocations() (Locations, error) {
+	locations := Locations{}
+
+	locationsURL := "https://groupietrackers.herokuapp.com/api/locations"
+	resp, err := http.Get(locationsURL)
+	if err != nil {
+		return locations, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		decoder := json.NewDecoder(resp.Body)
+		err = decoder.Decode(&locations)
+		if err != nil {
+			return locations, err
+		}
+	} else {
+		return locations, fmt.Errorf("failed to get data: %s", resp.Status)
+	}
+
+	return locations, nil
 }

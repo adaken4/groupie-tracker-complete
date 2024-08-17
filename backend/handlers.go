61820021f2284ff749e12d83a024c7b6439bce6e
adaken4 <adakennedy@outlook.com>
2024-08-17 14:04:42 +0300

    implementing artistsHandler that will execute artists data to the index boilerplate

--- backend/handlers.go
+++ backend/handlers.go
@@ -1,4 +1,22 @@
 package backend
 
-func homeHandler() {
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func artistsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		artists := GetAndUnmarshalArtists()
+		tempFile := "frontend/index.html"
+		temp, err := template.ParseFiles(tempFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = temp.Execute(w, artists)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }

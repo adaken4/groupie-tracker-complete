c544e2b1a539c34c4581c8e4a908d88f25e1d665
adaken4 <adakennedy@outlook.com>
2024-08-17 14:09:22 +0300

    implementing a multiplexer that registers the routes for the project

--- backend/routes.go
+++ backend/routes.go
@@ -1,4 +1,12 @@
 package backend
 
-func homeRoute() {
+import (
+	"net/http"
+)
+
+func RegisterRoutes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("frontend"))))
+	mux.HandleFunc("/artists", artistsHandler)
+	return mux
 }

5892efeb50108879a377a473cf5fc9367890e339
adaken4 <adakennedy@outlook.com>
2024-08-20 21:48:04 +0300

    refractored the code to add more helper functions to reduce redundancy, and added variables to cache retrived json data

--- backend/utils.go
+++ backend/utils.go
@@ -11,92 +11,102 @@ var client = &http.Client{
 	Timeout: 10 * time.Second,
 }
 
+var (
+	artistsData   json.RawMessage
+	locationsData json.RawMessage
+	datesData     json.RawMessage
+	relationData  json.RawMessage
+)
+
 const APIURL = "https://groupietrackers.herokuapp.com/api"
 
-func GetAndUnmarshalArtists() ([]Artists, error) {
-	artists := []Artists{}
-
-	jsonData, err := getJSONData(APIURL + "/artists")
+// getJSONData fetches JSON data from the API and returns it as a RawMessage
+func getJSONData(endpoint string) (json.RawMessage, error) {
+	resp, err := client.Get(APIURL + endpoint)
 	if err != nil {
-		return artists, err
-	}
-	json.Unmarshal(jsonData, &artists)
-
-	return artists, nil
-}
-
-func GetAndUnmarshalLocations(Id int) (Location, error) {
-	locations := Locations{}
-	location := Location{}
-
-	jsonData, err := getJSONData(APIURL + "/locations")
-	if err != nil {
-		return location, err
-	}
-	json.Unmarshal(jsonData, &locations)
-
-	for _, v := range locations.Index {
-		if v.ID == Id {
-			location = v
-		}
-	}
-
-	return location, nil
-}
-
-func GetAndUnmarshalDates(Id int) (Date, error) {
-	dates := Dates{}
-	date := Date{}
-
-	jsonData, err := getJSONData(APIURL + "/dates")
-	if err != nil {
-		return date, err
-	}
-	json.Unmarshal(jsonData, &dates)
-	for _, v := range dates.Index {
-		if v.ID == Id {
-			date = v
-		}
-	}
-
-	return date, nil
-}
-
-func GetAndUnmarshalRelation(Id int) (ArtistDetails, error) {
-	relation := Relation{}
-	artistDetails := ArtistDetails{}
-
-	jsonData, err := getJSONData(APIURL + "/relation")
-	if err != nil {
-		return artistDetails, err
-	}
-
-	json.Unmarshal(jsonData, &relation)
-	for _, v := range relation.Index {
-		if v.ID == Id {
-			artistDetails = v
-		}
-	}
-
-	return artistDetails, nil
-}
-
-func getJSONData(url string) (json.RawMessage, error) {
-	resp, err := client.Get(url)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get json data: %w", err)
+		return nil, fmt.Errorf("failed to get %s json data: %w", endpoint, err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("received a non-200 response code: %d", resp.StatusCode)
+		return nil, fmt.Errorf("received a non-200 response code for %s: %d", endpoint, resp.StatusCode)
 	}
 
-	var body json.RawMessage
-	err = json.NewDecoder(resp.Body).Decode(&body)
+	var jsonString json.RawMessage
+	err = json.NewDecoder(resp.Body).Decode(&jsonString)
 	if err != nil {
-		return nil, fmt.Errorf("failed to decode json data: %w", err)
+		return nil, fmt.Errorf("failed to decode %s json data: %w", endpoint, err)
 	}
 
-	return body, nil
+	return jsonString, nil
+}
+
+// unmarshalData is a helper function to unmarshal cached JSON data or fetch it if not available
+func unmarshalData(cache *json.RawMessage, endpoint string, out interface{}) error {
+	if *cache != nil {
+		return json.Unmarshal(*cache, out)
+	}
+
+	jsonData, err := getJSONData(endpoint)
+	if err != nil {
+		return err
+	}
+
+	*cache = jsonData
+	return json.Unmarshal(jsonData, out)
+}
+
+// GetAndUnmarshalArtists returns a list of artists by fetching or using cached data
+func GetAndUnmarshalArtists() ([]Artists, error) {
+	artists := []Artists{}
+	err := unmarshalData(&artistsData, "/artists", &artists)
+	return artists, err
+}
+
+// GetAndUnmarshalLocations returns a specific location by its ID
+func GetAndUnmarshalLocations(ID int) (Location, error) {
+	locations := Locations{}
+	err := unmarshalData(&locationsData, "/locations", &locations)
+	if err != nil {
+		return Location{}, err
+	}
+
+	for _, v := range locations.Index {
+		if v.ID == ID {
+			return v, nil
+		}
+	}
+	return Location{}, fmt.Errorf("location with ID %d not found", ID)
+}
+
+// GetAndUnmarshalDates returns specific dates by their ID
+func GetAndUnmarshalDates(ID int) (Date, error) {
+	dates := Dates{}
+	err := unmarshalData(&datesData, "/dates", &dates)
+	if err != nil {
+		return Date{}, err
+	}
+
+	for _, v := range dates.Index {
+		if v.ID == ID {
+			return v, nil
+		}
+	}
+	return Date{}, fmt.Errorf("date with ID %d not found", ID)
+}
+
+// GetAndUnmarshalRelation returns artist details by their ID
+func GetAndUnmarshalRelation(ID int) (ArtistDetails, error) {
+	relation := Relation{}
+	err := unmarshalData(&relationData, "/relation", &relation)
+	if err != nil {
+		return ArtistDetails{}, err
+	}
+
+	for _, v := range relation.Index {
+		if v.ID == ID {
+			return v, nil
+		}
+	}
+	return ArtistDetails{}, fmt.Errorf("relation with ID %d not found", ID)
 }

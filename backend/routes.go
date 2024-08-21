204ee88e409e019a61eba6d9dbc347f2036304bc
adaken4 <adakennedy@outlook.com>
2024-08-21 13:21:39 +0300

    adding locations and dates routes pending their handler functions

--- backend/routes.go
+++ backend/routes.go
@@ -8,6 +8,8 @@ func RegisterRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("frontend"))))
 	mux.HandleFunc("/", artistsHandler)
-	mux.HandleFunc("/artist", artistDetailsHandler)
+	// mux.HandleFunc("/locations", locationsHandler)
+	// mux.HandleFunc("/dates", datesHandler)
+	mux.HandleFunc("/relation", artistDetailsHandler)
 	return mux
 }
